d61bb222b20de4331a193ba96cd4e5ee83844cd5
kevinsbstn4 <kevinsbstn4@gmail.com>
2018-12-26 12:54:42 +0700

    perpustakaan

---
+++ src/pages/AuthorCreate.js
@@ -0,0 +1,8 @@
+import React,{Component} from 'react'
+export default class AuthorCreate extends Component{
+    render(){
+        return(
+            <h1>ini AuthorCreate</h1>
+        )
+    }
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react'
+import Book from './Book'
+export default class Home extends Component{
+    render(){
+        return(
+            <div>
+               <h1>Selamat Datang</h1>
+            </div>
+        )
+    }
+}
